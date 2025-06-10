7b98ae4dc4c1b127d23bf7470b9e8d1711dfac37
kaoutarel <kaoutar.elazzab@gmail.com>
2025-06-10 00:08:12 -0400

    fixing the shapes and scrolling issues in the home page

--- src/components/home/HeaderSocials.jsx
+++ src/components/home/HeaderSocials.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const HeaderSocials = () => {
   return (
     <div className="home__socials">
-      <a href="" className="home__social-link" target="_blank">
+      <a href="www.linkedin.com" className="home__social-link" target="_blank">
         <i className="fa-brands fa-linkedin"></i>
       </a>
-      <a href="" className="home__social-link" target="_blank">
+      <a href="#" className="home__social-link" target="_blank">
         <i className="fa-brands fa-github"></i>
       </a>
-      <a href="" className="home__social-link" target="_blank">
+      <a href="#" className="home__social-link" target="_blank">
         <i className="fa-brands fa-behance"></i>
       </a>
     </div>
